84a4265bdcf6c1a6d4ee0f3fad16812cf7434314
hss-smee <seanmee8186@gmail.com>
2020-02-21 12:32:56 -0800

    section 13 Dynamically Generating Pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,40 @@
 import React from "react";
+import {graphql, useStaticQuery} from "gatsby";
 import Layout from "../components/layout";
 
 const Blog = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+          allMarkdownRemark {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  date
+                }
+              }
+            }
+          }
+        }
+    `);
+
+    const posts = data.allMarkdownRemark.edges.map((p, i) => {
+        return (
+            <li key={i}>
+                <h2>{p.node.frontmatter.title}</h2>
+                <p>{p.node.frontmatter.date}</p>
+            </li>
+        )
+    });
+
     return (
         <Layout>
             <h1>Blog</h1>
             <p>Posts will show up here later on.</p>
+            <ol>
+                {posts}
+            </ol>
         </Layout>
     )
 };
